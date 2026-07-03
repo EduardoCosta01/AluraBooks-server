45e5a684c712b38ca63d948ea7ca47433831f9e4
Eduardo Costa01 <eccosta2000@bol.com.br>
2026-07-02 22:22:29 -0300

    instalacao do cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const rotaLivros = require('./rotas/livros')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/livros', rotaLivros)
 
